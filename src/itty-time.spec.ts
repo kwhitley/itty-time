c3600436974347e770402a4c52e28939a09166d6
Kevin Whitley <krwhitley@gmail.com>
2022-10-22 17:53:07 -0500

    initial codebase and tests

---
+++ src/itty-time.spec.ts
@@ -0,0 +1,119 @@
+import { describe, expect, it } from 'vitest'
+import {
+  getDatePlus,
+  getDuration,
+  getTTL,
+  divideBy,
+  divide,
+} from './itty-time'
+import * as time from './itty-time'
+
+describe('itty-time', () => {
+  describe('exports', () => {
+    it('getDuration', () => {
+      expect(typeof getDuration).toBe('function')
+    })
+
+    it('getTTL', () => {
+      expect(typeof getTTL).toBe('function')
+    })
+
+    it('getDatePlus', () => {
+      expect(typeof getDatePlus).toBe('function')
+    })
+  })
+
+  describe('getDuration(duration: string): { value: number, unit: string }', () => {
+    const tests = [
+      ['5 seconds', { value: 5, unit: 'second' }],
+      ['1 minutes', { value: 1, unit: 'minute' }],
+      ['24 hour', { value: 24, unit: 'hour' }],
+      ['321 day', { value: 321, unit: 'day' }],
+      ['12 week', { value: 12, unit: 'week' }],
+      ['3 month', { value: 3, unit: 'month' }],
+      ['11 year', { value: 11, unit: 'year' }],
+      ['1.5 minutes', { value: 1.5, unit: 'minute' }],
+    ]
+
+    it('returns value in { value: number, unit: string } format, such as { value: 12, unit: "minute" }', () => {
+      const { value, unit} = getDuration('5 minutes')
+
+      expect(value).toBe(5)
+      expect(unit).toBe('minute')
+    })
+
+    describe('parses', () => {
+      for (const [duration, expected] of tests) {
+        it(`getDuration('${duration}') => ${JSON.stringify(expected).replace(/([:,])/g, '$1 ')}`, () => {
+          const { value, unit } = getDuration(duration)
+
+          expect(getDuration(duration)).toEqual(expected)
+        })
+      }
+    })
+  })
+
+  describe('getTTL(duration: string): number', () => {
+    type TTLTest = [duration: string, expected: number]
+
+    const tests: TTLTest[] = [
+      ['5 seconds', 5],
+      ['1 minutes', 60],
+      ['24 hour', 60 * 60 * 24],
+      ['321 day', 60 * 60 * 24 * 321],
+    ]
+
+    describe('returns number of seconds', () => {
+      for (const [duration, expected] of tests) {
+        it(`getTTL('${duration}') => ${expected}`, () => {
+          expect(getTTL(duration)).toEqual(expected)
+        })
+      }
+    })
+  })
+
+  describe('divideBy(seconds: number) => (duration: string): number', () => {
+    type DivisionTest = [
+      duration: string,
+      divisor: string,
+      result: number,
+      fuzzy?: boolean,
+    ]
+
+    const tests: DivisionTest[] = [
+      ['1 week', 'day', 7],
+      ['2 minutes', 'second', 120],
+      ['3 days', 'hour', 24 * 3],
+      ['1 day', '3 hours', 8],
+      ['1 year', 'month', 12, true],
+      ['4 months', 'weeks', 4.25 * 4, true],
+      ['3 minutes', 'seconds', 60 * 3],
+      ['1 week', 'seconds', 60 * 60 * 24 * 7],
+      ['24 hours', 'minutes', 60 * 24],
+      ['3 days', 'hours', 24 * 3],
+      ['1 day, 30 minutes', 'hours', 24 + 0.5],
+      ['1 week', 'days', 7],
+      ['12 hours', 'days', 0.5],
+      ['3 months', 'days', 30.5 * 3, true],
+      ['1 year', 'weeks', 52, true],
+      ['1 month', 'weeks', 4.3, true],
+      ['3 years', 'months', 36, true],
+    ]
+
+    describe('divides dates', () => {
+      for (const [duration, divisor, expected, fuzzy] of tests) {
+        it(`divide('${duration}').by('${divisor}') = ${fuzzy ? '~' : ''}${expected}`, () => {
+          const value = divide(duration).by(divisor)
+
+          if (fuzzy) {
+            const proximity = Math.min(value, expected) / Math.max(value, expected)
+            const closeEnough = proximity > 0.95
+            expect(closeEnough).toBe(true)
+          } else {
+            expect(value).toBe(expected)
+          }
+        })
+      }
+    })
+  })
+})
---
+++ src/itty-time.ts
@@ -0,0 +1,70 @@
+// FUNCTION: get { value, unit } from a duration string
+export const getDuration = (duration: string) => {
+  const match = duration.match(/^(?<strValue>[\d.]+)?\s?(?<unit>\w+?)s?$/)
+  const { strValue, unit } = match.groups
+  const value = +(strValue === undefined ? 1 : strValue)
+
+  return { value, unit }
+}
+
+// FUNCTION: get future date from a duration string (e.g. getDatePlus('3 hours'))
+export const getDatePlus = (duration: string) => {
+  const durations = duration.split(/,?\s*and\s*|,\s*/)
+  let next = new Date()
+
+  for (const d of durations) {
+    const { value, unit } = getDuration(d)
+    const conversions = { 'Days': 'Date', 'Weeks': 'Date', 'Years': 'Month', 'Months': 'Month' }
+    const multipliers = { 'Weeks': 7, 'Years': 12 }
+    let capUnit = unit.charAt(0).toUpperCase() + unit.slice(1) + 's'
+    const multiple = multipliers[capUnit] || 1
+    capUnit = conversions[capUnit] || capUnit
+    const [get, set] = [`get${capUnit}`, `set${capUnit}`]
+    next = new Date(next[set](next[get]() + value * multiple))
+  }
+
+  return next
+}
+
+// FUNCTION: get number of seconds from a duration string
+export const getTTL = (duration: string) => {
+  const now = +(new Date)
+  const next = +(getDatePlus(duration))
+
+  return (next - now) / 1000|0
+}
+
+// HELPER FUNCTION: creates convenience methods below
+export const divideBy = (divisor: string) => {
+  const seconds = getTTL(divisor)
+
+  return (duration: string) => {
+    const now = +(new Date()) / 1000|0
+    const next = +(getDatePlus(duration)) / 1000|0
+    const diff = next - now
+
+    return diff / seconds
+  }
+}
+
+// HELPER FUNCTION: creates convenience methods below
+export const divide = (duration: string) => ({
+  by: (divisor: string) => {
+    const now = +(new Date()) / 1000|0
+    const next = +(getDatePlus(duration)) / 1000|0
+    const diff = next - now
+
+    return diff / getTTL(divisor)
+  }
+})
+
+// export const getSeconds = d => divide(d).by('seconds')
+
+// // convenience methods
+// // export const getSeconds = divideBy('seconds')
+// export const getMinutes = divideBy('minutes')
+// export const getHours = divideBy('hours')
+// export const getDays = divideBy('days')
+// export const getWeeks = divideBy('weeks')
+// export const getMonths = divideBy('months')
+// export const getYears = divideBy('years')
