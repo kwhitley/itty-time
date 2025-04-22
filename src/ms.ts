1c08dbca59cb20a322db2bbc2011cb63fbd06a70
Caio Nogueira <caio@cloudflare.com>
2025-04-22 15:34:31 +0100

    Fix: ms breaking when called with 0

--- src/ms.ts
+++ src/ms.ts
@@ -2,7 +2,7 @@ import { units } from './lib/units'
 
 // FUNCTION: get number of seconds from a duration string
 export const ms = (duration: string | number): number => {
-  if (+duration) return +duration
+  if (+duration === duration) return +duration
   // @ts-ignore
   const [, value, unit] = duration.match(/^([^ ]+) +(\w\w*?)s?$/) || []
 
